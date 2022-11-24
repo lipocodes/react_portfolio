fcf4eab9932a7a56aecd06672ae0d40dab70771e
Lior <lipocodes@yahoo.com>
2022-11-24 14:18:02 +0200

    2nd commit

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,18 +3,28 @@ import './topBar.scss';
 import {Person,Mail} from '@material-ui/icons'
 
 
-const Topbar = () => {
+const Topbar = ({menuOpen,setMenuOpen}) => {
   return (
-    <div className='topBar'>
+    <div className={'topBar '+ (menuOpen && 'active')}>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className='logo'>Genius</a>
-          <div className="itemConta">
-            <Person/>
+          <a href="#intro" className='logo'>Service.</a>
+          <div className="itemContainer">
+            <Person className="icon"/>
             <span>+972 54 4968835</span>
           </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>Lipocodes@gmail.com</span>
+          </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="hamburger" onClick={()=>{setMenuOpen(!menuOpen)}}>
+            <span className='line1'></span>
+            <span className='line2'></span>
+            <span className='line3'></span>
+          </div>
+        </div>
       </div>
     </div>
   )
