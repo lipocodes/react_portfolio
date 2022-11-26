7204f1e2aebb51cfbb591a65c77a0fd8a00d6bab
Lior <lipocodes@yahoo.com>
2022-11-26 11:10:17 +0200

    4th  commit

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,9 +1,63 @@
 import React from 'react'
 import './works.scss';
+import imgMobile from '../../assets/mobile.png';
+import imgArrow from '../../assets/arrow.png';
 
 const Works = () => {
+  const data = [
+    {
+      id: "1",
+      icon: "./assets/mobile.png",
+      title: "Web Design",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      img:
+        "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+    },
+    {
+      id: "2",
+      icon: "./assets/globe.png",
+      title: "Mobile Application",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img:
+        "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+    },
+    {
+      id: "3",
+      icon: "./assets/writing.png",
+      title: "Branding",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img:
+        "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+    },
+  ];
+
   return (
-    <div className='works' id='works'>Works</div>
+    <div className='works' id='works'>
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src={imgMobile} alt=""/>
+                </div>
+                <h2>Title</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.  </p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className="right">
+              <img  src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"  alt="new"/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <img src={imgArrow} className="arrow left" alt=""/>
+      <img src={imgArrow} className="arrow right" alt=""/>
+    </div>
   )
 }
 
--- src/data.jsx
+++ src/data.jsx
@@ -1,97 +1,191 @@
+
+
 export const featuredPortfolio = [
     {
-      id: "banking",
-      title: "Banking App",
-      img:"https://www.digitaltrends.com/wp-content/uploads/2019/05/best-mobile-banking-tips-citi-7852.jpg?fit=1053%2C702&p=1"
+      id: 1,
+      title: "Social Media App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
     {
-      id: "banking",
-      title: "Banking App",
-      img:"https://www.digitaltrends.com/wp-content/uploads/2019/05/best-mobile-banking-tips-citi-7852.jpg?fit=1053%2C702&p=1"
+      id: 2,
+      title: "Rampa UI Design",
+      img:
+        "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
     },
     {
-     id: "banking",
-     title: "Banking App",
-     img:"https://www.digitaltrends.com/wp-content/uploads/2019/05/best-mobile-banking-tips-citi-7852.jpg?fit=1053%2C702&p=1"
+      id: 3,
+      title: "E-commerce Web Design",
+      img:
+        "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 4,
+      title: "Relax Mobile App",
+      img:
+        "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 5,
+      title: "Hero Web Design",
+      img:
+        "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 6,
+      title: "Banking App",
+      img:
+        "https://cdn.dribbble.com/users/3307260/screenshots/15468444/media/447b4501f7a145b05e11c70199a9c417.jpg?compress=1&resize=1200x900",
     },
   ];
-
-
   
   export const webPortfolio = [
     {
-      id: "domino",
-      title: "Domino App",
-      img:"https://sada.com/wp-content/uploads/2018/06/dominos-app-256x300.png"
+      id: 1,
+      title: "Web Social Media App",
+      img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
     },
     {
-        id: "domino",
-        title: "Domino App",
-        img:"https://sada.com/wp-content/uploads/2018/06/dominos-app-256x300.png"
-      },
-      {
-        id: "domino",
-        title: "Domino App",
-        img:"https://sada.com/wp-content/uploads/2018/06/dominos-app-256x300.png"
-      }, 
-  ]; 
-  
-  
+      id: 2,
+      title: "Web Rampa UI Design",
+      img: "https://cdn.dribbble.com/users/3967258/screenshots/15463803/media/4fddb9a2caf3b3bd634060f706a91e73.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 3,
+      title: "Web E-commerce Design",
+      img: "https://cdn.dribbble.com/users/3637068/screenshots/15467010/media/69a3279182b75365013fe285733d1122.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 4,
+      title: "Web Relax App",
+      img: "https://cdn.dribbble.com/users/2264844/screenshots/15463998/media/d85e92a332c41094f68e1f716884f7ce.jpg?compress=1&resize=1200x900",
+    },
+    {
+      id: 5,
+      title: "Web Design",
+      img: "https://cdn.dribbble.com/users/387658/screenshots/15458608/media/e92ec1dd33dd1170ae3dc9c5272f2693.jpg?compress=1&resize=1200x900",
+    },
+    {
+      id: 6,
+      title: "Web Banking App",
+      img: "https://cdn.dribbble.com/users/6384483/screenshots/15468426/media/ce9479fa1f8dba3a4a49840d76e55e31.png?compress=1&resize=1200x900",
+    },
+  ];
   
   export const mobilePortfolio = [
     {
-      id: "chess",
-      title: "Chess App",
-      img:"https://images.chesscomfiles.com/uploads/v1/article/16004.dd880c32.668x375o.aeda77eba6e8.png"
+      id: 1,
+      title: "Mobile Social Media App",
+      img:
+        "https://cdn.dribbble.com/users/2597268/screenshots/15468893/media/44313553d73ba41580f1df69749bba28.jpg?compress=1&resize=1200x900",
     },
     {
-        id: "chess",
-        title: "Chess App",
-        img:"https://images.chesscomfiles.com/uploads/v1/article/16004.dd880c32.668x375o.aeda77eba6e8.png"
-      },
-      {
-        id: "chess",
-        title: "Chess App",
-        img:"https://images.chesscomfiles.com/uploads/v1/article/16004.dd880c32.668x375o.aeda77eba6e8.png"
-      },
-  ]; 
-  
-  
+      id: 2,
+      title: "Mobile Rampa UI Design",
+      img:
+        "https://cdn.dribbble.com/users/5920881/screenshots/15463142/media/b5a460223798bd85d835710806e2f3dd.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 3,
+      title: "Mobile E-commerce Design",
+      img:
+        "https://cdn.dribbble.com/users/1998175/screenshots/15459384/media/48ac2b43ebe81ba0866afea1383cc939.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 4,
+      title: "Mobile Relax App",
+      img:
+        "https://cdn.dribbble.com/users/2083704/screenshots/15468619/media/cd958306c7a772449e1ac23bd65ce506.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 5,
+      title: "Mobile Hero Design",
+      img:
+        "https://cdn.dribbble.com/users/26642/screenshots/15462545/media/1a202ef5c8338b6f78634e6edc1ba350.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 6,
+      title: "Mobile Banking App",
+      img:
+        "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
+    },
+  ];
   
   export const designPortfolio = [
     {
-      id: "soccer",
-      title: "Soccer App",
-      img:"https://streamdiag.com/wp-content/uploads/2019/06/soccer-app-on-smartphone-768x370.jpg"
+      id: 1,
+      title: "Design Social Media App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
     {
-        id: "soccer",
-        title: "Soccer App",
-        img:"https://streamdiag.com/wp-content/uploads/2019/06/soccer-app-on-smartphone-768x370.jpg"
-      },
-      {
-        id: "soccer",
-        title: "Soccer App",
-        img:"https://streamdiag.com/wp-content/uploads/2019/06/soccer-app-on-smartphone-768x370.jpg"
-      },
-  ];  
-
-
+      id: 2,
+      title: "Design Rampa UI Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 3,
+      title: "Design E-commerce Web Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 4,
+      title: "Design Relax Mobile App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 5,
+      title: "Design Keser Web Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 6,
+      title: "Design Banking App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+  ];
   
   export const contentPortfolio = [
     {
-      id: "banketball",
-      title: "Basketball App",
-      img:"https://image.winudf.com/v2/image1/Y29tLmRyb2lkaGVuLmJhc2tldGJhbGxfc2NyZWVuXzFfMTU2Njk5NTMxNl8wODE/screen-1.jpg?h=355&fakeurl=1&type=.jpg"
+      id: 1,
+      title: "Content Social Media App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
     {
-        id: "banketball",
-        title: "Basketball App",
-        img:"https://image.winudf.com/v2/image1/Y29tLmRyb2lkaGVuLmJhc2tldGJhbGxfc2NyZWVuXzFfMTU2Njk5NTMxNl8wODE/screen-1.jpg?h=355&fakeurl=1&type=.jpg"
-      },
-      {
-        id: "banketball",
-        title: "Basketball App",
-        img:"https://image.winudf.com/v2/image1/Y29tLmRyb2lkaGVuLmJhc2tldGJhbGxfc2NyZWVuXzFfMTU2Njk5NTMxNl8wODE/screen-1.jpg?h=355&fakeurl=1&type=.jpg"
-      },
-  ];  
+      id: 2,
+      title: "Content Rampa UI Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 3,
+      title: "Content E-commerce Web Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 4,
+      title: "Content Relax Mobile App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 5,
+      title: "Content Keser Web Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 6,
+      title: "Content Banking App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+  ];
+  
